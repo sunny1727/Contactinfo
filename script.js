2a9a33b9f43cf12a96ba225f30ef3fa5ecf806ce
sunny1727 <sunnesingh7104@gmail.com>
2024-10-16 17:14:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    const successMessage = document.getElementById('successMessage');
+    successMessage.classList.remove('hidden');
+    this.reset();
+});
